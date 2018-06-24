41826e2660a2a742c5cc565f690ecf795c15c0ff
androidneha <neha.8xx@gmail.com>
2018-06-24 17:20:32 +0530

    Remove travis badge.

---
+++ README.md
@@ -0,0 +1,4 @@
+Testing CI Using Travis
+=============================
+
+![license](https://img.shields.io/github/license/mashape/apistatus.svg)
---
+++ app/Controllers/Core/Web/Pages.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers\Core\Web;
+
+class Pages
+{
+    public function render() {
+        return 'Hello World';
+    }
+
+    public function returnTrue() {
+        return true;
+    }
+
+    public function returnArray() {
+        return array('Hello', 'World', 'This', 'Is', 'An', 'Array');
+    }
+}
